cfb2c83c73df5e6b6dc2dc4ded52727a415cab29
Debjoty Mitra <96808014+debjotyms@users.noreply.github.com>
2024-12-05 22:18:02 +0000

    Add space for components to the root page for showcasing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import AddButton from "@/components/buttons/add-button";
+import CancelButton from "@/components/buttons/cancel-button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Root Page</div>
+    <>
+      <div className='h-12 bg-black m-2 rounded-md'>
+        <div className='h-12 flex justify-center items-center text-lg'>
+          <span className='text-white font-bold'>Buttons</span>
+        </div>
+      </div>
+
+      <div className="flex items-center flex-col">
+        <AddButton text="Add me please!"/>
+        <CancelButton/>
+      </div>
+      
+      <div className='h-12 bg-black m-2 rounded-md mt-10'>
+        <div className='h-12 flex justify-center items-center text-lg'>
+          <span className='text-white font-bold'>Pop-ups</span>
+        </div>
+      </div>  
+    </>
   );
 }
