3ab1e951f3f46d3834c22c9a50b0b20bec96ef3b
Debjoty Mitra <debjotyms@gmail.com>
2024-12-06 14:12:26 +0600

    Replace Trash2 icon with Ban icon in CancelButton component

--- src/components/buttons/cancel-button.tsx
+++ src/components/buttons/cancel-button.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Trash2 } from 'lucide-react';
+import { Ban } from 'lucide-react';
 import { Button } from "@/components/ui/button"
 
 const CancelButton: React.FC = () => {
     return (
         <div className=''>
             <Button variant={'secondary'} className="">
-                <Trash2/> Cancel
+                <Ban/> Cancel
             </Button>
         </div>
     );
