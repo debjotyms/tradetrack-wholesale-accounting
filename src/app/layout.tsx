be89f2145c1e60344bf4d7acc49a1fd2a7b49184
Debjoty Mitra <debjotyms@gmail.com>
2024-12-20 17:23:48 +0600

    feat: Implement Main Navigation component and update layout structure; add new navigation menu dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./globals.css";
-import {AppSidebar} from "@/components/others/app-sidebar";
+import { MainNav } from "@/components/ui/main-nav";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,14 +9,13 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import {Separator} from "@/components/ui/separator";
-import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <head>
@@ -24,31 +23,10 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        <SidebarProvider>
-          <AppSidebar />
-          <main className="w-full">
-            <SidebarInset>
-              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                <div className="flex items-center gap-2 px-4">
-                  <SidebarTrigger className="-ml-1" />
-                  <Separator orientation="vertical" className="mr-2 h-4" />
-                  <Breadcrumb>
-                    <BreadcrumbList>
-                      <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#">TradeTrack</BreadcrumbLink>
-                      </BreadcrumbItem>
-                      <BreadcrumbSeparator className="hidden md:block" />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Components</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </BreadcrumbList>
-                  </Breadcrumb>
-                </div>
-              </header>
-              <div>{children}</div>
-            </SidebarInset>
-          </main>
-        </SidebarProvider>
+        <MainNav />
+        <main className="w-full p-4">
+          <div>{children}</div>
+        </main>
       </body>
     </html>
   );
